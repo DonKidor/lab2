55b926c738a4d2b69a3987e33fda0f959815ea46
Misha <Misha@DESKTOP-BAMHVS7>
2022-02-19 17:47:34 +0700

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+  
+
+class Program {
+    public static void Main(String[] args) {
+        const int n = 12;
+        Random rand = new Random();
+        RomanNumber[] b = new RomanNumber[n];
+        for (int i = 0; i < n; i++)
+        {
+            ushort x = (ushort)(rand.NextDouble() * 2000);
+            b[i] = new RomanNumber(x);
+            Console.WriteLine(b[i].ToString() + " " + x);
+        }
+
+        Array.Sort<RomanNumber>(b);
+        Console.WriteLine();
+
+        foreach (RomanNumber r in b)
+        {
+            Console.WriteLine(r.ToString());
+        }
+    }
+}
